0470cdfaf29050cda1dfe4dd4640085149c71380
Diego Antoni <diegoantoni13@gmail.com>
2020-03-27 22:33:11 -0300

    Mobile da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,12 +3,12 @@ module.exports = {
     async index(request, response){
         const { page = 1 } = request.query;
 
-        const count = await connection('incidents').count();
+        const [count] = await connection('incidents').count();
         
         const incidents = await connection('incidents')
         .join('ongs','ongs.id', '=', 'incidents.ong_id')
         .limit(5)
-        .offset((page -1) * 5)
+        .offset((page - 1) * 5)
         .select([
             'incidents.*',
             'ongs.name',
